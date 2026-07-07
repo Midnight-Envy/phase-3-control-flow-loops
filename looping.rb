5df5af5eb57b01ffab46a361e121cf412f7781a2
Thomas Correia <thomas.correiap@gmail.com>
2026-07-07 14:07:39 -0400

    Completed Control Flow Loops

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
 def happy_new_year
   # your code here
+  count = 10
+
+  until count == 0
+    puts count
+    count -= 1
+  end
+
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +25,24 @@ end
 
 def fizzbuzz_printer
   # your code here
+  count = 1
+
+  until count > 100
+    puts fizzbuzz(count)
+    count += 1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed = ""
+
+  index = str.length - 1
+
+  until index < 0
+    reversed += str[index]
+    index -= 1
+  end
+
+  reversed
 end
